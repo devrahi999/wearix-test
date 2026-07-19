74b11673a52a33f5cba0722f9acc630ae20467a3
devrahi999 <mdrahirahman9999@gmail.com>
2026-07-19 12:03:20 +0600

    n

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#ffffff',
     theme_color: '#ffffff',
     icons: [
-      { src: '/icon', sizes: '32x32', type: 'image/png' }
+      { src: '/favicon.ico', sizes: 'any', type: 'image/x-icon' }
     ],
   };
 }
